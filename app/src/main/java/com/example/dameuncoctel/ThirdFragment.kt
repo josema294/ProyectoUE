cac2fa78004eb266b0cf7b58b974b365c4549b24
Jose Maria Aranguren <jmaranguren89@gmail.com>
2023-04-15 13:17:08 +0200

    [15-04] Estructura basica de viewpager, desplazamiento funcional pero sin contenido ni formato

--- app/src/main/java/com/example/dameuncoctel/ThirdFragment.kt
+++ app/src/main/java/com/example/dameuncoctel/ThirdFragment.kt
@@ -6,17 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.dameuncoctel.databinding.FragmentFirstBinding
+import com.example.dameuncoctel.databinding.FragmentSecondBinding
+import com.example.dameuncoctel.databinding.FragmentThirdBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class ThirdFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    lateinit var _binding: FragmentThirdBinding
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,19 +22,17 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+
     }
 }
