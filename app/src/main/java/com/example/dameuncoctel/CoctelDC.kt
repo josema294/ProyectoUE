749edde92f938343cba319ef3c040f5af6518aa5
Jose Maria Aranguren <jmaranguren89@gmail.com>
2023-04-18 21:31:46 +0200

    "Se integra el recyclerview dentro del FirstFragment, pero con datos hardcodeados, no integracion con base de datos "

---
+++ app/src/main/java/com/example/dameuncoctel/CoctelDC.kt
@@ -0,0 +1,3 @@
+package com.example.dameuncoctel
+
+data class CoctelDC(var id: Int, val nombre: String, val foto: Int )
--- app/src/main/java/com/example/dameuncoctel/FirstFragment.kt
+++ app/src/main/java/com/example/dameuncoctel/FirstFragment.kt
@@ -1,11 +1,14 @@
 package com.example.dameuncoctel
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dameuncoctel.databinding.FragmentFirstBinding
 
 /**
@@ -14,6 +17,10 @@ import com.example.dameuncoctel.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
+    lateinit var listaCocteles: ArrayList<CoctelDC>
+    lateinit var recycler: RecyclerView
+    lateinit var adaptadorRecycler: AdaptadorRecycler
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,6 +32,25 @@ class FirstFragment : Fragment() {
     ): View? {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+
+        //Creo lista de cocteles de prueba para testeo
+        //TODO cambiar lista de cocteles de prueba por cocteles de la base de datos
+        listaCocteles = ArrayList()
+        listaCocteles.add(CoctelDC(1, "Mojito", R.drawable.mojito))
+        listaCocteles.add(CoctelDC(2, "Margarita", R.drawable.margarita))
+        listaCocteles.add(CoctelDC(3, "Caipiriña", R.drawable.caipirinha))
+        listaCocteles.add(CoctelDC(4, "Martini", R.drawable.martini))
+        listaCocteles.add(CoctelDC(5, "Piña Colada", R.drawable.pinacolada))
+        listaCocteles.add(CoctelDC(6, "Daiquiri", R.drawable.daiquiri))
+
+        recycler = binding.recyclerCocktails
+        recycler.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+        adaptadorRecycler = AdaptadorRecycler(requireContext(), listaCocteles)
+        recycler.adapter = adaptadorRecycler //  asignar el adaptador al RecyclerView
+
         return binding.root
 
     }
