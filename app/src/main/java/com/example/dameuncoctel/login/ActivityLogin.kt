2272fbfe4d4908941b54adcdb34e4dc74388a824
Jose Maria Aranguren <jmaranguren89@gmail.com>
2023-04-21 12:23:46 +0200

    Agregada pagina de logeo, layous basicos, funcionalidad no implementada

--- app/src/main/java/com/example/dameuncoctel/login/ActivityLogin.kt
+++ app/src/main/java/com/example/dameuncoctel/login/ActivityLogin.kt
@@ -2,13 +2,33 @@ package com.example.dameuncoctel.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.viewpager.widget.ViewPager
+import com.example.dameuncoctel.AdaptadorPager
 import com.example.dameuncoctel.R
+import com.google.android.material.tabs.TabLayout
 
 class ActivityLogin : AppCompatActivity() {
+
+
+    lateinit var adaptadorPagerLogin:AdaptadorPagerLogin
+    lateinit var viewPagerLogin: ViewPager
+    lateinit var tabs: TabLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        //creo instancia del pager
+
+        adaptadorPagerLogin = AdaptadorPagerLogin (supportFragmentManager)
+        viewPagerLogin = findViewById(R.id.view_pagerLogin)
+        tabs = findViewById(R.id.tabLogin)
+
+        //configuracion de pager
+
+        viewPagerLogin.setAdapter(adaptadorPagerLogin)
 
 
 
--- app/src/main/java/com/example/dameuncoctel/login/AdaptadorPAgerLogin.kt
+++
@@ -1,4 +0,0 @@
-package com.example.dameuncoctel.login
-
-class AdaptadorPAgerLogin {
-}
