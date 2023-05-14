0ba57c0697ec0284f9cdca7404526a3f04cc01de
Jose Maria Aranguren <jmaranguren89@gmail.com>
2023-05-14 13:44:31 +0200

    Boton de favoritos funcional, agrega y desagrega a BD, falta pulido.

--- app/src/main/java/com/example/dameuncoctel/coctel/CoctelActivity.kt
+++ app/src/main/java/com/example/dameuncoctel/coctel/CoctelActivity.kt
@@ -12,7 +12,14 @@ import com.example.dameuncoctel.R
 import com.example.dameuncoctel.databinding.ActivityCoctelBinding
 import com.example.dameuncoctel.model.CoctelDC
 import com.example.dameuncoctel.model.Ingrediente
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class CoctelActivity : AppCompatActivity() {
 
@@ -22,6 +29,8 @@ class CoctelActivity : AppCompatActivity() {
     private lateinit var adaptador: AdaptadorPagerCoctel
     private lateinit var viewPager: ViewPager
     private lateinit var bundle: Bundle
+    private lateinit var fab: FloatingActionButton
+    private var coctel: CoctelDC? = null
     lateinit var tabs: TabLayout
 
 
@@ -43,6 +52,8 @@ class CoctelActivity : AppCompatActivity() {
         adaptador = AdaptadorPagerCoctel(supportFragmentManager, bundle,this)
         viewPager = findViewById(R.id.view_pager_coctel)
         tabs = findViewById(R.id.tab_coctel)
+        fab = binding.includeCoctel.fab
+
 
         //Hacemos las configuraciones de los elementos
         setSupportActionBar(toolbar)
@@ -52,14 +63,20 @@ class CoctelActivity : AppCompatActivity() {
         //Cambiamos el titulo del coctel al que pasamos del recycler y
         // recuperamos datos de el recycler con el coctel seleccionado
 
-        val coctel: CoctelDC? = bundle?.get("coctel") as CoctelDC?
+        coctel = bundle?.get("coctel") as CoctelDC?
 
         Log.d("bundle traido a CoctelActivity", bundle.toString())
         Log.d("coctel traido a CoctelActivity", coctel.toString())
 
         binding.includeCoctel.textViewTitulococtel.text = coctel?.strDrink
         if (coctel != null) {
-            Glide.with(applicationContext).load(coctel.strDrinkThumb).into(binding.includeCoctel.imageView3)
+            Glide.with(applicationContext).load(coctel!!.strDrinkThumb).into(binding.includeCoctel.imageView3)
+        }
+
+        fab.setOnClickListener {
+
+            botonnFab()
+
         }
 
 
@@ -67,6 +84,38 @@ class CoctelActivity : AppCompatActivity() {
         //binding.includeCoctel.imageView3.setImageResource(coctel?.foto ?: R.drawable.caipirinha)
 
 
+
+
+    }
+
+
+    private fun botonnFab () {
+
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        val cocktailId = coctel?.idDrink
+
+
+        if (userId != null) {
+            val databaseReference = FirebaseDatabase.getInstance().getReference("Usuario/$userId/Favoritos/$cocktailId")
+
+            databaseReference.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        // Si el coctel ya está en favoritos, lo eliminamos
+                        databaseReference.removeValue()
+                        Snackbar.make(binding.root,"prueba coctel ya existe, se limino de favoritos",1000).show()
+                    } else {
+                        // Si el coctel no está en favoritos, lo añadimos
+                        databaseReference.setValue(cocktailId)
+                        Snackbar.make(binding.root,"prueba se agrega el coctel a favoritos",1000).show()
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Aquí se manejan los errores
+                }
+            })
+        }
 
 
     }
