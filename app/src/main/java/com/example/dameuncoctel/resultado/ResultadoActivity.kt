61d37b33b5e0d63ec8344f77f0ab2d789b02bdb4
Jose Maria Aranguren <jmaranguren89@gmail.com>
2023-05-15 16:08:07 +0200

    Reduccion de warnings

--- app/src/main/java/com/example/dameuncoctel/resultado/ResultadoActivity.kt
+++ app/src/main/java/com/example/dameuncoctel/resultado/ResultadoActivity.kt
@@ -2,18 +2,17 @@ package com.example.dameuncoctel.resultado
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dameuncoctel.R
 import com.example.dameuncoctel.menu.MenuActivity
 import com.example.dameuncoctel.model.CoctelDC
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -32,16 +31,20 @@ class ResultadoActivity : AppCompatActivity() {
     lateinit var mRootReferenceCoctail: DatabaseReference
     private lateinit var query: Query
     private lateinit var seleccionCategoria: String
+    private lateinit var recyclerViewResultado: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
 
-        toolbar = findViewById(R.id.toolbar_2);
+        toolbar = findViewById(R.id.toolbar_2)
         setSupportActionBar(toolbar)
 
         //Se vacia el array de cocteles para que cada vez que se entre se escriban sobre solo los datos correspondientes
         arrayCocteles = ArrayList()
         arrayCocteles.clear()
+
+        recyclerViewResultado = findViewById(R.id.recyclerViewResultado)
 
 
         intent = getIntent()
@@ -53,9 +56,9 @@ class ResultadoActivity : AppCompatActivity() {
         //Recuperamos el array con el resultado de cocteles
         //TODO arreglar el deprecated
         if (bundle?.getSerializable("categoria") != null) {
-            seleccionCategoria = bundle?.getSerializable("categoria") as String
+            seleccionCategoria = bundle.getSerializable("categoria") as String
 
-            fun AñadirItemslist(listaCocteles: ArrayList<CoctelDC>): ArrayList<CoctelDC> {
+            fun AnadirItemslist(listaCocteles: ArrayList<CoctelDC>): ArrayList<CoctelDC> {
 
                 mRootReferenceCoctail = FirebaseDatabase.getInstance().getReference("coctail")
                 query = mRootReferenceCoctail.orderByKey()
@@ -67,37 +70,37 @@ class ResultadoActivity : AppCompatActivity() {
                     override fun onDataChange(snapshot: DataSnapshot) {
 
                         if (snapshot.exists()) {
-                            var ky: String = ""
-                            var itnm: String = ""
+                            var ky: String
+                            var itnm: String
                             //var limit: Int = 0
                             for (itmsnapshot in snapshot.children) {
                                 val item = itmsnapshot.getValue(CoctelDC::class.java)
                                 item!!.strIngredient?.lowercase()
-                                item!!.strIngredient2?.lowercase()
-                                item!!.strIngredient3?.lowercase()
-                                item!!.strIngredient4?.lowercase()
-                                item!!.strIngredient5?.lowercase()
-                                item!!.strIngredient6?.lowercase()
-                                item!!.strIngredient7?.lowercase()
-                                item!!.strIngredient8?.lowercase()
-                                item!!.strIngredient9?.lowercase()
-                                item!!.strIngredient10?.lowercase()
-                                if (item!!.strAlcoholic?.equals(seleccionCategoria) == true) {
-                                    listaCocteles.add(item!!)
+                                item.strIngredient2?.lowercase()
+                                item.strIngredient3?.lowercase()
+                                item.strIngredient4?.lowercase()
+                                item.strIngredient5?.lowercase()
+                                item.strIngredient6?.lowercase()
+                                item.strIngredient7?.lowercase()
+                                item.strIngredient8?.lowercase()
+                                item.strIngredient9?.lowercase()
+                                item.strIngredient10?.lowercase()
+                                if (item.strAlcoholic?.equals(seleccionCategoria) == true) {
+                                    listaCocteles.add(item)
                                     ky = itmsnapshot.key.toString()
                                     itnm = item.strDrink.toString()
                                     println(ky + " nombre " + itnm)
                                 } else {
-                                    if (item!!.strIngredient?.contains(seleccionCategoria) == true
-                                        || item!!.strIngredient2?.contains(seleccionCategoria) == true
-                                        || item!!.strIngredient3?.contains(seleccionCategoria) == true
-                                        || item!!.strIngredient4?.contains(seleccionCategoria) == true
-                                        || item!!.strIngredient5?.contains(seleccionCategoria) == true
-                                        || item!!.strIngredient6?.contains(seleccionCategoria) == true
-                                        || item!!.strIngredient7?.contains(seleccionCategoria) == true
-                                        || item!!.strIngredient8?.contains(seleccionCategoria) == true
-                                        || item!!.strIngredient9?.contains(seleccionCategoria) == true
-                                        || item!!.strIngredient10?.contains(seleccionCategoria) == true
+                                    if (item.strIngredient?.contains(seleccionCategoria) == true
+                                        || item.strIngredient2?.contains(seleccionCategoria) == true
+                                        || item.strIngredient3?.contains(seleccionCategoria) == true
+                                        || item.strIngredient4?.contains(seleccionCategoria) == true
+                                        || item.strIngredient5?.contains(seleccionCategoria) == true
+                                        || item.strIngredient6?.contains(seleccionCategoria) == true
+                                        || item.strIngredient7?.contains(seleccionCategoria) == true
+                                        || item.strIngredient8?.contains(seleccionCategoria) == true
+                                        || item.strIngredient9?.contains(seleccionCategoria) == true
+                                        || item.strIngredient10?.contains(seleccionCategoria) == true
                                     ) {
                                         listaCocteles.add(item!!)
                                         ky = itmsnapshot.key.toString()
@@ -111,11 +114,6 @@ class ResultadoActivity : AppCompatActivity() {
 
                             }
 
-
-                            /*limit = limit + 1
-                            if (limit >= 20) {
-                                break
-                            }*/
                             val recycler = findViewById<RecyclerView>(R.id.recyclerViewResultado)
                             recycler.layoutManager =
                                 LinearLayoutManager(contexto, LinearLayoutManager.VERTICAL, false)
@@ -136,7 +134,7 @@ class ResultadoActivity : AppCompatActivity() {
                 return listaCocteles
             }
             //Realizamos una query a la base de datos y lo añadimos a la lista de cocteles
-            AñadirItemslist(arrayCocteles)
+            AnadirItemslist(arrayCocteles)
 
 
         }
@@ -157,9 +155,9 @@ class ResultadoActivity : AppCompatActivity() {
         }
 
         if (bundle3 != null) {
-            var searchQuery = bundle3.getString("query", "default").lowercase().capitalize()
+            val searchQuery = bundle3.getString("query", "default").lowercase().capitalize()
 
-            var cocktailNames: ArrayList<CoctelDC> = ArrayList()
+            val cocktailNames: ArrayList<CoctelDC> = ArrayList()
 
 
             val database = FirebaseDatabase.getInstance().getReference("coctail")
@@ -304,9 +302,10 @@ class ResultadoActivity : AppCompatActivity() {
         // Aquí implementas el código para lanzar tu Activity de resultados.
         val intent = Intent(this, ResultadoActivity::class.java)
         val bundle = Bundle()
-        bundle.putString("query",query)
+        bundle.putString("query", query)
         intent.putExtra("bundlequery", bundle)
         startActivity(intent)
     }
+
 
 }
